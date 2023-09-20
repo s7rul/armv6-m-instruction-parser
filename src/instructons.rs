78d17dde0222be5ff7d6f7fccf25ad02148de20b
s7rul <serraerik1@gmail.com>
2023-09-20 13:33:32 +0200

    progress?

--- src/instructons.rs
+++ src/instructons.rs
@@ -4,6 +4,16 @@ pub enum Instruction {
     Bit32(Instruction32Bit),
 }
 
+impl Instruction {
+    pub fn is_16bit(&self) -> bool {
+        matches!(self, Instruction::Bit16(_))
+    }
+
+    pub fn is_32bit(&self) -> bool {
+        matches!(self, Instruction::Bit32(_))
+    }
+}
+
 #[derive(Debug)]
 pub enum Instruction16Bit {
     NOP,
@@ -11,5 +21,21 @@ pub enum Instruction16Bit {
 
 #[derive(Debug)]
 pub enum Instruction32Bit {
-    NOP,
+    MSR,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn instruction_size() {
+        let instruction_32 = Instruction::Bit32(Instruction32Bit::MSR);
+        assert_eq!(instruction_32.is_32bit(), true);
+        assert_eq!(instruction_32.is_16bit(), false);
+
+        let instruction_16 = Instruction::Bit16(Instruction16Bit::NOP);
+        assert_eq!(instruction_16.is_32bit(), false);
+        assert_eq!(instruction_16.is_16bit(), true);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 pub mod instructons;
 pub mod registers;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use instructons::*;
+use registers::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn parse(input: &[u8]) -> Result<Instruction, ()> {
+    if input.len() < 2 {
+        return Err(());
     }
+
+    match (input[0] & 0b11111) >> 10 {
+        0b11101 | 0b11110 | 0b11111 => {
+            // Check if it is a 32-bit instruction.
+            Ok(Instruction::Bit32(parse_32bit_instruction(input)?))
+        }
+        _ => Ok(Instruction::Bit16(parse_16bit_instruction(input)?)),
+    }
+}
+
+fn parse_32bit_instruction(input: &[u8]) -> Result<Instruction32Bit, ()> {
+    todo!()
+}
+
+fn parse_16bit_instruction(input: &[u8]) -> Result<Instruction16Bit, ()> {
+    todo!()
 }
