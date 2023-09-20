6c5ecf5cbe6905c6530c20877a5699810445e0d6
s7rul <serraerik1@gmail.com>
2023-09-20 14:13:37 +0200

    Reorganised instruction

--- src/instructons.rs
+++ src/instructons.rs
@@ -1,27 +1,28 @@
 #[derive(Debug)]
-pub enum Instruction {
-    Bit16(Instruction16Bit),
-    Bit32(Instruction32Bit),
+pub struct Instruction {
+    pub width: InstructionWidth,
+    pub operation: Operation,
+}
+
+#[derive(Debug)]
+pub enum InstructionWidth {
+    Bit32,
+    Bit16,
 }
 
 impl Instruction {
     pub fn is_16bit(&self) -> bool {
-        matches!(self, Instruction::Bit16(_))
+        matches!(self.width, InstructionWidth::Bit16)
     }
 
     pub fn is_32bit(&self) -> bool {
-        matches!(self, Instruction::Bit32(_))
+        matches!(self.width, InstructionWidth::Bit32)
     }
 }
 
 #[derive(Debug)]
-pub enum Instruction16Bit {
+pub enum Operation {
     NOP,
-}
-
-#[derive(Debug)]
-pub enum Instruction32Bit {
-    MSR,
 }
 
 #[cfg(test)]
@@ -30,11 +31,17 @@ mod test {
 
     #[test]
     fn instruction_size() {
-        let instruction_32 = Instruction::Bit32(Instruction32Bit::MSR);
+        let instruction_32 = Instruction {
+            width: InstructionWidth::Bit32,
+            operation: Operation::NOP,
+        };
         assert_eq!(instruction_32.is_32bit(), true);
         assert_eq!(instruction_32.is_16bit(), false);
 
-        let instruction_16 = Instruction::Bit16(Instruction16Bit::NOP);
+        let instruction_16 = Instruction {
+            width: InstructionWidth::Bit16,
+            operation: Operation::NOP,
+        };
         assert_eq!(instruction_16.is_32bit(), false);
         assert_eq!(instruction_16.is_16bit(), true);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,36 @@ pub fn parse(input: &[u8]) -> Result<Instruction, ()> {
         return Err(());
     }
 
-    match (input[0] & 0b11111) >> 10 {
+    match (input[0] & 0b11111) >> 2 {
         0b11101 | 0b11110 | 0b11111 => {
             // Check if it is a 32-bit instruction.
-            Ok(Instruction::Bit32(parse_32bit_instruction(input)?))
+            if input.len() < 4 {
+                return Err(());
+            };
+            let mut instruction_bytes: [u8; 4] = [0; 4];
+            instruction_bytes.copy_from_slice(&input[0..4]);
+            let instruction_bits = u32::from_le_bytes(instruction_bytes);
+            Ok(Instruction {
+                width: InstructionWidth::Bit32,
+                operation: parse_32bit_operation(instruction_bits)?,
+            })
         }
-        _ => Ok(Instruction::Bit16(parse_16bit_instruction(input)?)),
+        _ => {
+            let mut instruction_bytes: [u8; 2] = [0; 2];
+            instruction_bytes.copy_from_slice(&input[0..2]);
+            let instruction_bits = u16::from_le_bytes(instruction_bytes);
+            Ok(Instruction {
+                width: InstructionWidth::Bit16,
+                operation: parse_16bit_operation(instruction_bits)?,
+            })
+        }
     }
 }
 
-fn parse_32bit_instruction(input: &[u8]) -> Result<Instruction32Bit, ()> {
+fn parse_32bit_operation(input: u32) -> Result<Operation, ()> {
     todo!()
 }
 
-fn parse_16bit_instruction(input: &[u8]) -> Result<Instruction16Bit, ()> {
+fn parse_16bit_operation(input: u16) -> Result<Operation, ()> {
     todo!()
 }
